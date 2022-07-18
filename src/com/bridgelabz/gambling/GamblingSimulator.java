502d2c7fe5d91a9c86c58573ee45b5f8dccf3b47
rakeshrewale <rakeshrewale993@gmail.com>
2022-07-18 13:36:37 +0530

    Added: check total amount of 20 days

--- src/com/bridgelabz/gambling/GamblingSimulator.java
+++ src/com/bridgelabz/gambling/GamblingSimulator.java
@@ -4,25 +4,46 @@ import java.util.Random;
 
 public class GamblingSimulator {
 
-	public static final int initial_Stake = 100;
-	public static final int stake_Bet = 1;
+	public static final int stake = 100;
+	public static final int bet = 1;
+	public static final int percentage_50_Stke = (stake / 100) * 50;
+	public static final int high_Limit = stake + percentage_50_Stke;
+	public static final int low_Limit = stake - percentage_50_Stke;
+	public static final int start_Day = 1;
+	public static final int max_Day = 20;
+	public static final int stakes_Per_Day = 50;
+	public static final int start_Dollars = 0;
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to the Gambling Simulation Program");
+		System.out.println("Welcome to the Gambler Game");
+		int days = start_Day;
+		int totalDollars = start_Dollars;
+		for (days = start_Day; days <= max_Day; days++) {
 
-		int totalStake = 0;
+			int totalStake = stake;
 
-		Random random = new Random();
-		int game = random.nextInt(2);
-		if (game == 1) {
-			totalStake = totalStake + stake_Bet;
+			while (totalStake < high_Limit && totalStake > low_Limit) {
+				int game = (int) (Math.random() * 10) % 2;
+				if (game == 1) {
+					totalStake = totalStake + bet;
+					System.out.println("Gambler won the bet and having total stack " + totalStake);
+				} else {
+					totalStake = totalStake - bet;
+					System.out.println("Gambler lost the bet and having total stack " + totalStake);
+				}
+			}
+			if (totalStake == high_Limit) {
+				System.out.println("Gambler won by " + totalStake);
+			} else {
+				System.out.println("Gambler lost by " + totalStake);
 
-			System.out.println("Gambler won the bet and have total stake:" + totalStake);
-		} else {
-			totalStake = totalStake - stake_Bet;
+			}
 
-			System.out.println("Gambler lost the bet and have total stake:" + totalStake);
 		}
-
+		if (totalDollars > start_Dollars) {
+			System.out.println("Gambler won " + totalDollars);
+		} else {
+			System.out.println("Gambler lost " + totalDollars);
+		}
 	}
 }
