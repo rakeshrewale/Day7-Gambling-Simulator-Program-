794df2c2a8dfcee88f35f27bd35979770d7b421d
rakeshrewale <rakeshrewale993@gmail.com>
2022-07-18 13:11:01 +0530

    Added: starting the gambling game

--- src/com/brz/gambling/GamblingSimulator.java
+++ src/com/brz/gambling/GamblingSimulator.java
@@ -1,5 +1,15 @@
 package com.brz.gambling;
 
 public class GamblingSimulator {
+	
+	public static final int initial_Stake = 100;
+	public static final int stake_Bet = 1;
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Gambling Simulation Program");
+
+		System.out.println("Initial Stake is " + initial_Stake + "$");
+		System.out.println("Stake Bet is " + stake_Bet + "$");
+	}
 
 }
